03c2dfc6b1d373da890deaadb46ed41d5daf9ff9
Shezcute09 <chiemezieok3@gmail.com>
2025-01-22 10:50:33 +0100

    my first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <div>
+      <Html>
+        <Head />
+        <body className="antialised">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    </div>
+  );
+}
